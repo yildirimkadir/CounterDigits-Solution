0fb5331287d41b9f727a42b8846ae676d4a85390
yildirimkadir <yildirimkad34@gmail.com>
2022-06-05 14:57:45 +0200

    "Die Aufgabe wurde erledigt."

--- solution.js
+++ solution.js
@@ -23,3 +23,61 @@ for (let x of list) {
 
 console.log(list);
 console.log(count );
+
+
+// Solution ;
+
+function countDigits(digit, low, high) { 
+    let arr = [];
+    let count = 0;
+    if(digit < 10 && digit >=0 && low < high &&  digit <= high) {
+        for(let i = low; i <= high; i++) {
+            arr.push(i);
+        }
+        arr.forEach((x) => String(x).split("").forEach((y) => y === String(digit) && count++));
+        return count;
+    } 
+
+    return-1;
+}
+
+/* *** Tests *** */
+let desc = "reverse range";
+let inputDigit = 1;
+let inputLow = 3;
+let inputHigh = 2;
+let actual = countDigits(inputDigit, inputLow, inputHigh);
+let expected = -1;
+assertEqual(actual, expected, desc);
+
+desc = "digit gt 9";
+inputDigit = 10;
+inputLow = 2;
+inputHigh = 3;
+actual = countDigits(inputDigit, inputLow, inputHigh);
+expected = -1;
+assertEqual(actual, expected, desc);
+
+desc = "no match";
+inputDigit = 2;
+inputLow = 5;
+inputHigh = 10;
+actual = countDigits(inputDigit, inputLow, inputHigh);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = "3_30_39";
+inputDigit = 3;
+inputLow = 30;
+inputHigh = 39;
+actual = countDigits(inputDigit, inputLow, inputHigh);
+expected = 11;
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
